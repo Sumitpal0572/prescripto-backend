ea4b172e6d32b90722050211315181d937d6fd4d
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-06-13 12:12:24 +0530

    admin route created

---
+++ routes/adminRoute.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+    addDoctor,
+    loginAdmin,
+    allDoctors,
+    appointmentsAdmin,
+    appointmentCancel,
+    adminDashboard,
+} = require("../controllers/adminController");
+const upload = require("../middlewares/multer");
+const authAdmin = require("../middlewares/authAdmin.js");
+const { changeAvailability } = require("../controllers/doctorController.js");
+
+const adminRouter = express.Router();
+
+adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor); //add new doctor
+adminRouter.post("/login", loginAdmin); //admin login
+adminRouter.post("/all-doctors", authAdmin, allDoctors); //get all doctors
+adminRouter.post("/change-availability", authAdmin, changeAvailability); //change availability of doctor
+adminRouter.get("/appointments", authAdmin, appointmentsAdmin); //check doctors appointments
+adminRouter.post("/cancel-appointment", authAdmin, appointmentCancel); //cancel doctor appointments
+adminRouter.get("/dashboard", authAdmin, adminDashboard); //admin dashboard
+
+module.exports = adminRouter;
