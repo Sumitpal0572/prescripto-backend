cb8f012cc7e959404c53ebd6fef51c8b41b6ce8b
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-06-16 11:29:20 +0530

    doctorController created

---
+++ controllers/doctorController.js
@@ -0,0 +1,181 @@
+const doctorModel = require("../models/doctorModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const appointmentModel = require("../models/appointmentModel");
+
+// Change Availability of doctors
+const changeAvailability = async (req, res) => {
+    try {
+        const { docId } = req.body;
+        const docData = await doctorModel.findById(docId);
+        await doctorModel.findByIdAndUpdate(docId, {
+            available: !docData.available,
+        });
+        res.json({ success: true, message: "Availability changed" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+// Doctor list
+const doctorList = async (req, res) => {
+    try {
+        const doctors = await doctorModel.find({}).select("-password -email");
+        res.json({ success: true, doctors });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+//Api for doctor login
+const loginDoctor = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        //find doctor
+        const doctor = await doctorModel.findOne({ email });
+        if (!doctor) {
+            return res.json({ success: false, message: "Invalid credentials" });
+        }
+
+        //match password
+        const isMatch = await bcrypt.compare(password, doctor.password);
+        if (isMatch) {
+            //generate token
+            const token = jwt.sign({ id: doctor._id }, process.env.JWT_SECRET);
+            res.json({ success: true, token });
+        } else {
+            res.json({ success: false, message: "Invalid credentials" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+//Api to get doctor appointments for doctor panel
+const appointmentsDoctor = async (req, res) => {
+    try {
+        const { docId } = req.body;
+        const appointments = await appointmentModel.find({ docId });
+
+        res.json({ success: true, appointments });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+//Api to mark appointment completed for doctor panel
+const appointmentComplete = async (req, res) => {
+    try {
+        const { docId, appointmentId } = req.body;
+
+        //find appointment data
+        const appointmentData = await appointmentModel.findById(appointmentId);
+        if (appointmentData && appointmentData.docId === docId) {
+            await appointmentModel.findByIdAndUpdate(appointmentId, {
+                isCompleted: true,
+            });
+            return res.json({ success: true, message: "Appointment Completed" });
+        } else {
+            return res.json({ success: false, message: "Mark failed" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+//Api to cancel appointment for doctor panel
+const appointmentCancel = async (req, res) => {
+    try {
+        const { docId, appointmentId } = req.body;
+
+        //find appointment data
+        const appointmentData = await appointmentModel.findById(appointmentId);
+        if (appointmentData && appointmentData.docId === docId) {
+            await appointmentModel.findByIdAndUpdate(appointmentId, {
+                cancelled: true,
+            });
+            return res.json({ success: true, message: "Appointment Cancelled" });
+        } else {
+            return res.json({ success: false, message: "Cancellation failed" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+//Api to get dashboard data for doctor panel
+const doctorDashboard = async (req, res) => {
+    try {
+        const { docId } = req.body;
+        const appointments = await appointmentModel.find({ docId });
+        //doctor earning
+        let earnings = 0;
+        appointments.map((item) => {
+            if (item.isCompleted || item.payment) {
+                earnings += item.amount;
+            }
+        });
+
+        //get number of patients
+        let patients = [];
+        appointments.map((item) => {
+            if (!patients.includes(item.userId)) {
+                patients.push(item.userId);
+            }
+        });
+
+        //Dashboard data
+        const dashData = {
+            earnings,
+            appointments: appointments.length,
+            patients: patients.length,
+            latestAppointments: appointments.reverse().slice(0, 5), //latest 5 appointments
+        };
+        res.json({ success: true, dashData });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+//Api to get doctor profile for doctor panel
+const doctorProfile = async (req, res) => {
+    try {
+        const { docId } = req.body;
+        const profileData = await doctorModel.findById(docId).select("-password");
+        res.json({ success: true, profileData });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+//Api to update doctor profile from doctor panel
+const updateDoctorProfile = async (req, res) => {
+    try {
+        const { docId, fees, address, available } = req.body;
+        await doctorModel.findByIdAndUpdate(docId, { fees, address, available });
+        res.json({ success: true, message: "Profile updated" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+module.exports = {
+    changeAvailability,
+    doctorList,
+    loginDoctor,
+    appointmentsDoctor,
+    appointmentComplete,
+    appointmentCancel,
+    doctorDashboard,
+    updateDoctorProfile,
+    doctorProfile,
+};
